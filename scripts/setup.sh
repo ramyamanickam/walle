a9d6e4b0d1c583595d6744cf4fb34713abc1727a
ramyasm123 <ramyasm.walle@gmail.com>
2015-12-13 21:58:43 +0000

    Setup Instruction for lightpd

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,12 +5,18 @@
 #python setup.py install
 
 #To setup mysql-python-connector
-echo "Removing existing zip files"
-rm mysql-connector-python-2.0.4.zip
-rm -rf mysql-connector-python-2.0.4
-wget "http://cdn.mysql.com/Downloads/Connector-Python/mysql-connector-python-2.0.4.zip#md5=3df394d89300db95163f17c843ef49df"
-sleep 5
-unzip mysql-connector-python-2.0.4.zip
-cd mysql-connector-python-2.0.4
-echo "About to Install mysql-connector"
-sudo python setup.py install
+#echo "Removing existing zip files"
+#rm mysql-connector-python-2.0.4.zip
+#rm -rf mysql-connector-python-2.0.4
+#wget "http://cdn.mysql.com/Downloads/Connector-Python/mysql-connector-python-2.0.4.zip#md5=3df394d89300db95163f17c843ef49df"
+#sleep 5
+#unzip mysql-connector-python-2.0.4.zip
+#cd mysql-connector-python-2.0.4
+#echo "About to Install mysql-connector"
+#sudo python setup.py install
+
+#Install lighttpd server
+sudo apt-get install lighttpd
+echo @About to backup the config file"
+sudo cp /etc/lighttpd/lighttpd.conf /etc/lighttpd/lighttpd.conf.backup
+sudo cp /home/pi/git/walle/walle/config/etc/lighttpd/lighttpd.conf /etc/lighttpd/
---
+++ web_scripts/var/www/python-files/reminder.py
@@ -0,0 +1,3 @@
+import mysql.connector
+
+cnx=mysql.connector.connect(user='ramya', database='robot', password='manickam')
